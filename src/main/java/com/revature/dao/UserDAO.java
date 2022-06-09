77f708eb958ffe0b207fe011aefe8d4db44a5d58
Hal Coronado <hallie_coronado1@baylor.edu>
2022-06-09 14:39:09 -0500

    connected to hal rds

--- src/main/java/com/revature/dao/UserDAO.java
+++ src/main/java/com/revature/dao/UserDAO.java
@@ -1,7 +1,8 @@
 package com.revature.dao;
 
 import com.revature.models.Users;
-import com.revature.util.database.DatabaseConnection;
+import com.revature.util.database.ConnectionFactory;
+
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,23 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements ICrudDAO<Users>{
-    Connection con = DatabaseConnection.getCon();
+
 
     @Override
     public void save(Users obj) {
-        try{
-            PreparedStatement ps = con.prepareStatement("insert into ers_users(user_id, username, password, email, given_name, surname) values(?,?,?,?,?,?)");
-            ps.setString(1, obj.getUser_id());
-            ps.setString(2, obj.getUsername());
-            ps.setString(3, obj.getPassword());
-            ps.setString(4, obj.getEmail());
-            ps.setString(5, obj.getGiven_name());
-            ps.setString(6, obj.getSurname());
-            ps.executeUpdate();
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
+        try(Connection con = ConnectionFactory.getInstance().getConnection()){
+                PreparedStatement ps = con.prepareStatement("insert into ers_users(user_id, username, password, email, given_name, surname) values(?,?,?,?,?,?)");
+                ps.setString(1, obj.getUser_id());
+                ps.setString(2, obj.getUsername());
+                ps.setString(3, obj.getPassword());
+                ps.setString(4, obj.getEmail());
+                ps.setString(5, obj.getGiven_name());
+                ps.setString(6, obj.getSurname());
+                ps.executeUpdate();
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
         }
-    }
+
 
     @Override
     public void update(Users obj) {
@@ -46,7 +48,7 @@ public class UserDAO implements ICrudDAO<Users>{
 
     public List<String> getAllUsernames(){
         List<String> usernames = new ArrayList<>();
-        try{
+        try(Connection con = ConnectionFactory.getInstance().getConnection()){
             PreparedStatement ps = con.prepareStatement("select username from ers_users");
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -60,7 +62,7 @@ public class UserDAO implements ICrudDAO<Users>{
 
     public List<String> getAllEmails(){
         List<String> emails = new ArrayList<>();
-        try{
+        try(Connection con = ConnectionFactory.getInstance().getConnection()){
             PreparedStatement ps = con.prepareStatement("select email from ers_users");
             ResultSet rs = ps.executeQuery();
             while(rs.next()){
@@ -73,7 +75,7 @@ public class UserDAO implements ICrudDAO<Users>{
     }
 
     public boolean login(Users user){
-        try{
+        try(Connection con = ConnectionFactory.getInstance().getConnection()){
             PreparedStatement ps = con.prepareStatement("select * from ers_user where username = ? and password = ?");
             ps.setString(1,user.getUsername());
             ps.setString(2,user.getPassword());
@@ -88,7 +90,7 @@ public class UserDAO implements ICrudDAO<Users>{
 
     public Users getUsernameAndPassword(String username, String password){
         Users user = null;
-        try {
+        try (Connection con = ConnectionFactory.getInstance().getConnection()){
             PreparedStatement ps = con.prepareStatement("SELECT * FROM ers_users WHERE username = ? and password = ?");
             ps.setString(1,username);
             ps.setString(2,password);
